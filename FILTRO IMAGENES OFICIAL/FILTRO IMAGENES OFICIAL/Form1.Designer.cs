562d008e1da6317dacb2345ccb5bb05ad6bdee9a
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2022-12-14 17:04:21 -0400

    Add files via upload

---
+++ FILTRO IMAGENES OFICIAL/FILTRO IMAGENES OFICIAL/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace FILTRO_IMAGENES_OFICIAL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.groupBox1.Location = new System.Drawing.Point(360, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.groupBox1.Size = new System.Drawing.Size(459, 116);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Proceso Imagenes";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(169, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Abrir Imagen";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.InfoText;
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(309, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(131, 31);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Limpiar Imagen";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.InfoText;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(13, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 31);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Contorno Negro";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 149);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(590, 415);
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(608, 149);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(605, 415);
+            this.pictureBox2.TabIndex = 22;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.InfoText;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(161, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 31);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Contorno Blanco";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1225, 576);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ FILTRO IMAGENES OFICIAL/FILTRO IMAGENES OFICIAL/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FILTRO_IMAGENES_OFICIAL
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        int[, ,] matrix = new int[3, 5000 + 1, 5000 + 1];
+        int[, ,] new_matrix = new int[3, 5000 + 1, 5000 + 1];
+        int[,] mask = null;
+        int n = 3;
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+
+            button2.Text = "Desabilidado";
+            button2.BackColor = Color.Empty;
+            button3.Text = "Desabilidado";
+            button3.BackColor = Color.Empty;
+            button4.Text = "Desabilidado";
+            button4.BackColor = Color.Empty;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Todos|*.*|Archivos JPEG|*.jpg|Archivos GIF|*.gif";
+            openFileDialog1.FileName = "";
+            openFileDialog1.ShowDialog();
+            bmp = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = bmp;
+
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+
+            button2.Text = "Contorno Blanco";
+            button2.BackColor = Color.Black;
+            button3.Text = "Contorno Negro";
+            button3.BackColor = Color.Black;
+            button4.Text = "Limpiar Imagen";
+            button4.BackColor = Color.Black;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Color c = new Color();
+            String datos = "";
+            
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    c = bmp.GetPixel(i, j);
+                    matrix[0, i, j] = c.R;
+                    matrix[1, i, j] = c.G;
+                    matrix[2, i, j] = c.B;
+                }
+            }
+
+            //textBox1.Text = datos;
+            filtro_imagen();
+
+        }
+        private void filtro_imagen() {
+            mask = new int[,]{{0, 1, 0}, {1, -4, 1}, {0, 1, 0} };
+            Color c = new Color();
+            Bitmap bmp2 = new Bitmap(bmp.Width, bmp.Height);
+            for (int i = 0; i < bmp.Width; i++) {
+                for (int j = 0; j < bmp.Height; j++) {
+                    if(i > 0 && i + 1 < bmp.Width && j > 0 && j + 1 < bmp.Height) {
+                        int value = 0;
+                        for (int k = 0; k < 3; k++) { // para el color RGB
+                            for (int i2 = 0; i2 < n; i2++) {  // mult matrix
+                                for (int j2 = 0; j2 < n; j2++) { // mult matrix
+                                    value += mask[i2, j2] * matrix[k, i - 1 + i2, j - 1 + j2];
+                                }
+                            }
+                            if (value < 0)
+                                value = 0;
+                            if (value > 255)
+                                value = 255;
+                            new_matrix[k, i, j] = value;
+                        }
+
+                        bmp2.SetPixel(i, j, Color.FromArgb(new_matrix[0, i, j], new_matrix[0, i, j], new_matrix[0, i, j]));
+                    }else {
+                        for (int k = 0; k < 3; k++) {
+                            new_matrix[k, i, j] = matrix[k, i, j];
+                        }
+                        bmp2.SetPixel(i, j, Color.FromArgb(new_matrix[0, i, j], new_matrix[0, i, j], new_matrix[0, i, j]));
+                    }
+                }
+            }
+
+            pictureBox2.Image = bmp2;
+        }
+        private void filtro_imagen2()
+        {
+            mask = new int[,] { { 0, 1, 0 }, { 1, -4, 1 }, { 0, 1, 0 } };
+            Color c = new Color();
+            Bitmap bmp2 = new Bitmap(bmp.Width, bmp.Height);
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    if (i > 0 && i + 1 < bmp.Width && j > 0 && j + 1 < bmp.Height)
+                    {
+                        int value = 0;
+                        for (int k = 0; k < 3; k++)
+                        { // para el color RGB
+                            for (int i2 = 0; i2 < n; i2++)
+                            {  // mult matrix
+                                for (int j2 = 0; j2 < n; j2++)
+                                { // mult matrix
+                                    value += mask[i2, j2] * matrix[k, i - 1 + i2, j - 1 + j2];
+                                }
+                            }
+                            if (value < 0)
+                                value = 0;
+                            if (value > 255)
+                                value = 255;
+                            new_matrix[k, i, j] = value;
+                        }
+
+                        bmp2.SetPixel(i, j, Color.FromArgb(255 - new_matrix[0, i, j], 255 - new_matrix[0, i, j], 255 - new_matrix[0, i, j]));
+                    }
+                    else
+                    {
+                        for (int k = 0; k < 3; k++)
+                        {
+                            new_matrix[k, i, j] = matrix[k, i, j];
+                        }
+                        bmp2.SetPixel(i, j, Color.FromArgb(new_matrix[0, i, j], new_matrix[0, i, j], new_matrix[0, i, j]));
+                    }
+                }
+            }
+
+            pictureBox2.Image = bmp2;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp2 = new Bitmap(bmp.Width, bmp.Height);
+            
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    bmp2.SetPixel(i, j, bmp.GetPixel(i, j));
+                }
+            }
+            pictureBox2.Image = bmp2;
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mask = new int[,] { { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 } };
+            Color c = new Color();
+            Bitmap bmp2 = new Bitmap(bmp.Width, bmp.Height);
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    if (i > 0 && i + 1 < bmp.Width && j > 0 && j + 1 < bmp.Height)
+                    {
+                        int value = 0;
+                        for (int k = 0; k < 3; k++)
+                        { // para el color RGB
+                            for (int i2 = 0; i2 < n; i2++)
+                            {  // mult matrix
+                                for (int j2 = 0; j2 < n; j2++)
+                                { // mult matrix
+                                    value += mask[i2, j2] * matrix[k, i - 1 + i2, j - 1 + j2];
+                                }
+                            }
+                            if (value < 0)
+                                value = 0;
+                            if (value > 255)
+                                value = 255;
+                            new_matrix[k, i, j] = value;
+                        }
+
+                        bmp2.SetPixel(i, j, Color.FromArgb(new_matrix[0, i, j], new_matrix[0, i, j], new_matrix[0, i, j]));
+                    }
+                    else
+                    {
+                        for (int k = 0; k < 3; k++)
+                        {
+                            new_matrix[k, i, j] = matrix[k, i, j];
+                        }
+                        bmp2.SetPixel(i, j, Color.FromArgb(new_matrix[0, i, j], new_matrix[0, i, j], new_matrix[0, i, j]));
+                    }
+                }
+            }
+
+            pictureBox2.Image = bmp2;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Color c = new Color();
+            String datos = "";
+
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    c = bmp.GetPixel(i, j);
+                    matrix[0, i, j] = c.R;
+                    matrix[1, i, j] = c.G;
+                    matrix[2, i, j] = c.B;
+                }
+            }
+
+            //textBox1.Text = datos;
+            filtro_imagen2();
+        }
+
+        
+    }
+}
